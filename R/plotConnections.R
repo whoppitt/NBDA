eaa8efeee785b599c4680e17e91df113a6f5e212
whoppitt <36671572+whoppitt@users.noreply.github.com>
2019-10-10 20:37:01 +0100

    Add files via upload

--- R/plotConnections.R
+++ R/plotConnections.R
@@ -1,5 +1,5 @@
-plotConnections<-function(nbdadata,network=1,rescale=F,lty=1, symbol=NULL,xlab="Acquisition event", ylab=NULL,title=NULL,plotID=NULL,offset=c(0.1,0),xlim=NULL, ylim=NULL, titlePos=c(0,0),
-                          averageLinelty=2,averageLinecol=4){
+plotConnections<-function(nbdadata,network=1,rescale=F,lty=1,lwd=1,col=2, symbol=NULL,xlab="Acquisition event", ylab=NULL,title=NULL,plotID=NULL,offset=c(0.1,0),xlim=NULL, ylim=NULL, titlePos=c(0,0),
+                          averageLinelty=2,averageLinecol=4,averagelwd=4){
 
   connections<-nbdadata@stMetric[,network]
   if(rescale){
@@ -15,18 +15,18 @@ plotConnections<-function(nbdadata,network=1,rescale=F,lty=1, symbol=NULL,xlab="
 
     if(is.null(symbol)){
       plot(nbdadata@time2,connections,col=nbdadata@status+1,xlab=xlab, ylab=ylab,main="",xlim=xlim, ylim=ylim);
-      points(nbdadata@time2[nbdadata@status==1],connections[nbdadata@status==1],col=2);
-      lines(nbdadata@time2[nbdadata@status==1],connections[nbdadata@status==1],col=2, lty=lty);
+      points(nbdadata@time2[nbdadata@status==1],connections[nbdadata@status==1],col=col);
+      lines(nbdadata@time2[nbdadata@status==1],connections[nbdadata@status==1],col=col, lty=lty,lwd=lwd);
 
     }else{
 
       plot(nbdadata@time2,connections,col=nbdadata@status+1,pch=as.numeric(as.factor(symbol)),xlab=xlab, ylab=ylab, main="",xlim=xlim, ylim=ylim)
-      points(nbdadata@time2[nbdadata@status==1],connections[nbdadata@status==1],col=2,pch=as.numeric(as.factor(symbol))[nbdadata@status==1]);
-      lines(nbdadata@time2[nbdadata@status==1],connections[nbdadata@status==1],col=2, lty=lty);
+      points(nbdadata@time2[nbdadata@status==1],connections[nbdadata@status==1],col=col,pch=as.numeric(as.factor(symbol))[nbdadata@status==1]);
+      lines(nbdadata@time2[nbdadata@status==1],connections[nbdadata@status==1],col=col, lty=lty,lwd=lwd);
     }
     if(!is.null(plotID)){
-      text(nbdadata@time2[nbdadata@status==1]+offset[1],connections[nbdadata@status==1]+offset[2],col=2,labels=plotID);
+      text(nbdadata@time2[nbdadata@status==1]+offset[1],connections[nbdadata@status==1]+offset[2],col=col,labels=plotID);
     }
     text(x=titlePos[1],y=titlePos[2],labels=title)
-    lines(unique(nbdadata@time2),tapply(connections,nbdadata@time2,mean),lty=averageLinelty,col=averageLinecol)
+    lines(unique(nbdadata@time2),tapply(connections,nbdadata@time2,mean),lty=averageLinelty,col=averageLinecol,lwd=averagelwd)
 }
